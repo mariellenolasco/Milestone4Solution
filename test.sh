667a807c93e23b73a8420c511629a273ff9dd9fd
mariellenolasco <marielle.nolasco@revature.com>
2020-08-31 18:10:18 -0400

    edited bash script to check for "essential" tasks

--- test.sh
+++ test.sh
@@ -9,20 +9,75 @@ then
     echo "Build Status:"
     scount=$(grep -c 'succeeded' response.txt)
     if [ $scount -gt 0 ] 
-    then 
+    then      
+        missing=0
         echo "Success!" 
         echo "Checking yaml file"
-        build=$(grep -c 'build' azure-pipelines.yml)
+        echo "Checking for build command"
+        build=$(grep -c 'dotnet build' azure-pipelines.yml)
         if [ $build -gt 0 ]
         then 
             #check if yml contains build command check if contains other commands
-            echo "pipeline has build command"
+            echo "Pipeline has build command"
         else
             echo "build command not found, pipeline flawed"
-        fi  
+            $missing++
+        fi
+        echo "Checking for testing"
+        test=$(grep -c 'dotnet test' azure-pipelines.yml)
+        if [ $test -gt 0]
+        then    
+            echo "Pipeline has testing"
+        else
+            echo "test command not found, pipeline flawed"
+            $missing++
+        fi
+        echo "Checking if sonar cloud is set up"
+        sonarc=$(grep -c 'SonarCloudPrepare@1' azure-pipelines.yml)
+        if [ $sonarc -gt 0]
+        then    
+            echo "Pipeline has sonar cloud set up"
+        else
+            echo "sonar cloud set up not found, pipeline flawed"
+            $missing++
+        fi
+        echo "Checking if code analysis was run"
+        codeanal=$(grep -c 'SonarCloudAnalyze@1' azure-pipelines.yml)
+        if [ $codeanal -gt 0]
+        then    
+            echo "Pipeline runs code analysis"
+        else
+            echo "code analysis run task not found, pipeline flawed"
+            $missing++
+        fi
+        echo "Checking if code analysis was published"
+        codeanalpub=$(grep -c 'SonarCloudPublish@1' azure-pipelines.yml)
+        if [ $codeanal -gt 0]
+        then    
+            echo "Pipeline publishes code analysis"
+        else
+            echo "code analysis publish task not found, pipeline flawed"
+            $missing++
+        fi 
+        echo "Checking if webapp was deployed"
+        deployed=$(grep -c 'AzureRmWebAppDeployment@4' azure-pipelines.yml)
+        if [ $deployed -gt 0]
+        then    
+            echo "Pipeline deploys webapp"
+        else
+            echo "webapp wasn't deployed"
+            $missing++
+        fi
+        if [$missing -gt 0]
+        then
+            echo "More than one essential task missing from pipeline, Fail!"
+        else    
+            echo "Passed!"
+        fi
     else
         echo "Failed!"
     fi
+
 else
 echo "Server unresponsive. Please check if your credentials are valid"
 fi
