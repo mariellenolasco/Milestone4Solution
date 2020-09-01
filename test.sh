90e96db0bd5c4f6d80ec5a237b5ab0af96403b93
mariellenolasco <marielle.nolasco@revature.com>
2020-09-01 16:44:22 -0400

    fixed bash script if statements

--- test.sh
+++ test.sh
@@ -25,7 +25,7 @@ then
         fi
         echo "Checking for testing"
         test=$(grep -c 'dotnet test' azure-pipelines.yml)
-        if [ $test -gt 0]
+        if [ $test -gt 0 ]
         then    
             echo "Pipeline has testing"
         else
@@ -34,7 +34,7 @@ then
         fi
         echo "Checking if sonar cloud is set up"
         sonarc=$(grep -c 'SonarCloudPrepare@1' azure-pipelines.yml)
-        if [ $sonarc -gt 0]
+        if [ $sonarc -gt 0 ]
         then    
             echo "Pipeline has sonar cloud set up"
         else
@@ -43,7 +43,7 @@ then
         fi
         echo "Checking if code analysis was run"
         codeanal=$(grep -c 'SonarCloudAnalyze@1' azure-pipelines.yml)
-        if [ $codeanal -gt 0]
+        if [ $codeanal -gt 0 ]
         then    
             echo "Pipeline runs code analysis"
         else
@@ -52,7 +52,7 @@ then
         fi
         echo "Checking if code analysis was published"
         codeanalpub=$(grep -c 'SonarCloudPublish@1' azure-pipelines.yml)
-        if [ $codeanal -gt 0]
+        if [ $codeanal -gt 0 ]
         then    
             echo "Pipeline publishes code analysis"
         else
@@ -61,14 +61,14 @@ then
         fi 
         echo "Checking if webapp was deployed"
         deployed=$(grep -c 'AzureRmWebAppDeployment@4' azure-pipelines.yml)
-        if [ $deployed -gt 0]
+        if [ $deployed -gt 0 ]
         then    
             echo "Pipeline deploys webapp"
         else
             echo "webapp wasn't deployed"
             $missing++
         fi
-        if [$missing -gt 0]
+        if [ $missing -gt 0 ]
         then
             echo "More than one essential task missing from pipeline, Fail!"
         else    
